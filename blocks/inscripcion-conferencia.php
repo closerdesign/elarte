f3391e35cfa1066901b84ff0855e381b22cc2b51
Juan Rodriguez <juanc@closerdesign.co>
2015-05-31 17:56:27 -0500

    Ajuste video https

--- blocks/inscripcion-conferencia.php
+++ blocks/inscripcion-conferencia.php
@@ -18,7 +18,7 @@
 			<div class="col-md-6">
 				<div class="row">
 					<div class="col-md-12">
-						<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/yHwo5EunKXo?autoplay=true' frameborder='0' allowfullscreen></iframe></div>
+						<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/yHwo5EunKXo?autoplay=true' frameborder='0' allowfullscreen></iframe></div>
 					</div>
 				</div>
 				<hr>
