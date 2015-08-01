5e65d625ea93db97df8139774bbcc49fc7613870
Luis Paloma <luispaloma3@gmail.com>
2015-08-01 15:41:31 -0500

    Se agregó el código para cambiar la visualización de los programas especiales

--- blocks/programas-especiales.php
+++ blocks/programas-especiales.php
@@ -1,12 +1,165 @@
 <div class="row top">
 	<div class="container">
-		<div class="col-md-8">
+		<div class="col-md-12">
 			<?php if(!isset($_REQUEST['alias'])){ ?>
 			<div class="row">
 				<div class="col-md-12">
+					<h2>Programas especiales</h2>
+					<p>Estos programas son espacios dirigidos por especialistas reconocidos en diversas áreas de.</p>
 					<p class="lead">Por favor seleccione uno de nuestros programas:</p>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="card">
+								<div class="front">
+									<div class="cover">
+										<img src="/images/rotating_card_thumb2.png">
+									</div>
+									<div class="user">
+										<img class="img-circle" src="/images/reflexiones.jpg">
+									</div>
+									<div class="content">
+										<div class="main">
+											<h3 class="name"><strong>Reflexiones para Vivir Mejor</strong></h3>
+											<p class="profession">Walter Riso</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-9 DescripcionPrograma">
+							<h3 class="DescripcionPrograma-titulo">Reflexiones para vivir mejor</h3>
+							<span class="DescripcionPrograma-subTitulo">Por Dr. Walter Riso (Psicologo y escritor)</span>
+							<br>
+							<p>
+								Programa en video conducido por el reconocido psicólogo y escritor Walter Riso en el que se busca crear un espacio de reflexión sobre los comportamientos que hacen parte de nuestra vida cotidiana.
+							</p>
+							<br>
+							<p>
+								<strong>Sobre Walter Riso:</strong> Nació en Italia (Nápoles). Es Doctor en psicología, especialista en Terapia Cognitiva y Magister en Bioética. Hace treinta años trabaja como psicólogo clínico y formador de terapeutas, práctica que alterna con el ejercicio de la catedra universitaria en Latinoamérica y España, y la publicación de textos cientificos y de divulgación, en diversos medios. Ha publicado más de veinte libros entre textos técnicos y de divulgación, muchos de ellos traducidos a más de 10 idiomas.
+							</p>
+							<div class="row">
+								<div class="col-lg-offset-10 col-lg-2">
+									<a href="/index.php?content=mi-cuenta&task=reflexiones" class="btn btn-success btn-lg pull-right">Ingresar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="card">
+								<div class="front">
+									<div class="cover">
+										<img src="/images/rotating_card_thumb2.png">
+									</div>
+									<div class="user">
+										<img class="img-circle" src="/images/iris.jpg">
+									</div>
+									<div class="content">
+										<div class="main">
+											<h3 class="name"><strong>Mente Sana, Vida Sana</strong></h3>
+											<p class="profession">Iris Luna</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-9 DescripcionPrograma">
+							<h3 class="DescripcionPrograma-titulo">Mente sana, vida sana</h3>
+							<span class="DescripcionPrograma-subTitulo">Por Dra. Iris Luna (Psiquiatra y nutricionista)</span>
+							<br>
+							<p>
+								Armonía entre cuerpo y mente. En este espacio la Dra. Iris Luna pretende a través de artículos, documentos, videos y otros materiales poner al alcance de nuestra comunidad herramientas y pautas para alcanzar un balance de bienestar entre cuerpo y mente.
+							</p>
+							<br>
+							<p>
+								<strong>Sobre Iris Luna:</strong> Iris Luna es médico psiquiatra, especialista en adicciones, magister en nutrición y especialista en sobrepeso y obesidad. Desde hace quince años trabaja como psicoterapeuta, docente y conferencista. Ha publicado cuatro libros académicos, así como múltiples artículos para revistas científicas sobre temas de psicopatología femenina. Dentro de sus actividades cabe destacar aquellas relacionadas con los temas de salud y prevención en salud mental y trastornos del comportamiento.
+							</p>
+							<div class="row">
+								<div class="col-lg-offset-10 col-lg-2">
+									<a href="index.php?content=programas-especiales&alias=mente-sana-vida-sana" class="btn btn-success btn-lg pull-right">Ingresar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="card">
+								<div class="front">
+									<div class="cover">
+										<img src="/images/rotating_card_thumb2.png">
+									</div>
+									<div class="user">
+										<img class="img-circle" src="/img/Luis_Florez_Alarcon.jpg">
+									</div>
+									<div class="content">
+										<div class="main">
+											<h3 class="name"><strong>¿Ángeles caídos o antropoides erguidos?</strong></h3>
+											<p class="profession">Luis Flórez Alarcón</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-9 DescripcionPrograma">
+							<h3 class="DescripcionPrograma-titulo">¿Ángeles caídos o antropoides erguidos?</h3>
+							<span class="DescripcionPrograma-subTitulo">Por Dr. Luis Flórez Alarcón (Psicólogo)</span>
+							<br>
+							<p>
+								Esta columna pretende analizar las motivaciones humanas a la luz de algunos principios científicos acerca del proceso motivacional, mirando este proceso desde una perspectiva teórica de corte cognitivo.
+							</p>
+							<br>
+							<p>
+								<strong>Sobre Luis Flórez:</strong> Psicólogo, Magister en Análisis Experimental de la Conducta y Doctor en Psicología Experimental. Actualmente es profesor Titular, adscrito al Departamento de Psicología de la Universidad Nacional de Colombia y director de la línea de investigación sobre Modelos Psicosociales en Prevención y Promoción de la Salud, en el grupo de investigación Estilo de Vida y Desarrollo Humano. Director del Centro de Estudios e Investigaciones sobre Adicciones y Violencia (CEIAV) de la Universidad Católica de Colombia.
+							</p>
+							<div class="row">
+								<div class="col-lg-offset-10 col-lg-2">
+									<a href="index.php?content=programas-especiales&alias=angeles-caidos-o-antropoides-erguidos" class="btn btn-success btn-lg pull-right">Ingresar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="card">
+								<div class="front">
+									<div class="cover">
+										<img src="/images/rotating_card_thumb2.png">
+									</div>
+									<div class="user">
+										<img class="img-circle" src="/img/Nancy_Castrillon.png">
+									</div>
+									<div class="content">
+										<div class="main">
+											<h3 class="name"><strong>El arte y la virtud del cuidado</strong></h3>
+											<p class="profession">Nancy Castrillón</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-9 DescripcionPrograma">
+							<h3 class="DescripcionPrograma-titulo">El arte y la virtud del cuidado</h3>
+							<span class="DescripcionPrograma-subTitulo">Por Dra. Nancy Castrillón (Psicóloga)</span>
+							<br>
+							<p>
+								Cuidar es un arte. En este espacio la Dra. Nancy Castrillón, licenciada en psicología, máster en Neurorehabilitación y Humanidades; nos enseña sobre el arte de cuidar. "Cuidar de mi para poder cuidar de otros".
+							</p>
+							<br>
+							<p>
+								<strong>Sobre Nancy Castrillón:</strong> Psicóloga con especialización en Psicología clínica; máster en Humanidades, y máster en Neurorehabilitación y Enfermedades Neurodegenerativas.
+							</p>
+							<div class="row">
+								<div class="col-lg-offset-10 col-lg-2">
+									<a href="index.php?content=programas-especiales&alias=el-arte-y-la-virtud-del-cuidado" class="btn btn-success btn-lg pull-right">Ingresar</a>
+								</div>
+							</div>
+						</div>
+					</div>
 					<?php
-						$sql = "
+					/*	$sql = "
 							SELECT
 								*
 							FROM
@@ -19,14 +172,14 @@
 						if($n<1){
 							echo('<p>No hay contenidos disponibles.</p>');
 						}else{
-							$html = "";
-							$html .= "<ul>";
+							$html = '';
+							$html .= '<div class="row">';
 							while($p = mysqli_fetch_assoc($q)){
 								$html .= "<li><a href='index.php?content=programas-especiales&alias=".$p['alias']."'>$p[titulo]</a></li>";
 							}
-							$html .= "</ul>";
+							$html .= '</div>';
 							echo $html;
-						}
+						}*/
 					?>
 				</div>
 			</div>
@@ -70,6 +223,7 @@
 				</div>
 			</div>
 			<div class="row">
+
 				<?php
 					$sql = "
 						SELECT
@@ -104,6 +258,6 @@
 			</div>
 			<?php } ?>
 		</div>
-		<?php require('blocks/articulos_sidebar.php'); ?>
+		<?php /*require('blocks/articulos_sidebar.php');*/ ?>
 	</div>
 </div>
