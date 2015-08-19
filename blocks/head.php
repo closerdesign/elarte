38d71c52740b1ab60721b77def3cbfadb2edd4ad
mac <mac@MacBook-Pro-de-mac.local>
2015-08-19 17:03:21 -0500

    Ajuste de url de favico para la seccion de articulos

--- blocks/head.php
+++ blocks/head.php
@@ -37,21 +37,31 @@
 	<link rel="stylesheet" href="/css/fws2.css" />
 	<link rel="stylesheet" href="/css/rotating-card.css" />
 	<link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+
+	<?php
+
+	if ( !empty( $_SERVER['HTTPS'] ) ) {
+		$url_icon = 'https://www.elartedesabervivir.com';
+	}else{
+		$url_icon = 'http://www.elartedesabervivir.com';
+	}
+
+	?>
 	
-	<link rel="apple-touch-icon" sizes="57x57" href="../images/icon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="../images/icon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="../images/icon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="../images/icon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="../images/icon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="../images/icon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="../images/icon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="../images/icon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="../images/icon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="../images/icon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="../images/icon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="../images/icon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="../images/icon/favicon-16x16.png">
-	<link rel="manifest" href="../images/icon/manifest.json">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?= $url_icon; ?>/images/icon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?= $url_icon; ?>/images/icon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?= $url_icon; ?>/images/icon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?= $url_icon; ?>/images/icon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?= $url_icon; ?>/images/icon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?= $url_icon; ?>/images/icon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?= $url_icon; ?>/images/icon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?= $url_icon; ?>/images/icon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?= $url_icon; ?>/images/icon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="<?= $url_icon; ?>/images/icon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?= $url_icon; ?>/images/icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?= $url_icon; ?>/images/icon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?= $url_icon; ?>/images/icon/favicon-16x16.png">
+	<link rel="manifest" href="<?= $url_icon; ?>/images/icon/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
