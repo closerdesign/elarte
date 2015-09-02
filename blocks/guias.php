34e284f79235be1785ff037442d4c9a2e93f900d
mac <mac@MacBook-Pro-de-mac.local>
2015-09-02 09:39:21 -0500

    Creación de nuevas peaginas para paquetes

---
+++ blocks/guias.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( !empty( $_REQUEST['id'] ) ) {
+	switch ( $_REQUEST['id'] ) {
+		case 1:
+			$_REQUEST['id'] = 1;
+			require 'guias1.php';
+			break;
+
+		case 2:
+			$_REQUEST['id'] = 1;
+			require 'guias2.php';
+			break;
+		case 3:
+			$_REQUEST['id'] = 1;
+			require 'guias3.php';
+			break;
+		default:
+			break;
+	}
+}
